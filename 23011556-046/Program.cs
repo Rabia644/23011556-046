64b38751d006f655f9a15065907401110f2925ee
UsEr <UsEr@DESKTOP-SO266LB>
2025-10-06 17:04:40 +0500

    Add project files.

---
+++ 23011556-046/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Question1 q1 = new Question1();
+            q1.Run();
+
+            Question2 q2 = new Question2();
+            q2.Run();
+
+            Question3 q3 = new Question3();
+            q3.Run();
+
+
+            Question4 q4 = new Question4();
+            q4.Run();
+
+
+            Question5 q5 = new Question5();
+            q5.Run();
+
+            Question6 q6 = new Question6();
+            q6.Run();
+
+            Question7 q7 = new Question7();
+            q7.Run();
+            Question8 q8 = new Question8();
+            q8.Run();
+            Question9 q9 = new Question9();
+            q9.Run();
+            Question10 q10 = new Question10();
+            q10.Run();
+
+        }
+    }
+}
+
---
+++ 23011556-046/program5.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Assignment1
+{
+    internal class Question5
+    {
+        public void Run()
+        {
+            Console.Write("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} * {i} = {n * i}");
+            }
+        }
+    }
+}
+
